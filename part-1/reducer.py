b833ebd976fab909065e7b9f5f051f99bd4e5e73
Sam Zhou <mascaradeemo@gmail.com>
2019-04-04 02:39:19 +1100

    reducer changed

--- part-1/reducer.py
+++ part-1/reducer.py
@@ -19,26 +19,24 @@ def reducer():
     """
     current_category = ""
     video_id_dict = {}
-    country_total = 0
 
     data = read_comb_output(sys.stdin)
     for category, video_id, country_sum in data:
         if current_category != category:
 
-            if current_category != "":
-                output = "{}: {}".format(current_category,round(len(video_id_dict[current_category])/country_total,2))
-                print(output.strip())
-
+            output = "{}: {}".format(current_category, round(video_id_dict[video_id]/len(video_id_dict), 2))
+            print(output.strip())
             current_category = category
             video_id_dict = {}
 
-        video_id_set = video_id_dict.get(current_category, set())
-        video_id_set.add(video_id)
-        video_id_dict[current_category] = video_id_set
-        country_total += int(country_sum)
+        # video_id_set = video_id_dict.get(current_category, set())
+        # video_id_set.add(video_id)
+        # video_id_dict[current_category] = video_id_set
+
+        video_id_dict[video_id] = video_id_dict.get(video_id, 0) + country_sum
 
     if current_category != "":
-        output = "{}: {}".format(current_category,round(len(video_id_dict[current_category])/country_total,2))
+        output = "{}: {}".format(current_category, round(video_id_dict[video_id]/len(video_id_dict), 2))
         print(output.strip())
 
 
